3fdfe7d7b58ebd7635e76ff9334fb52c58d10a9d
later <1638414737@qq.com>
2020-03-13 12:31:31 +0800

    Python 实战（2）：简单的数据库 finished

--- code.py
+++ code.py
@@ -1,21 +1,14 @@
-# coding:utf8
+# -*- coding: UTF-8 -*-
 import web
 import pymysql
-
-# MySql
-db = pymysql.connect("localhost", "root", "root", "moviesite")
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
-'''
-# 使用 execute()  方法执行 SQL 查询 
-cursor.execute("SELECT VERSION()")
-# 使用 fetchone() 方法获取单条数据.
-data = cursor.fetchone()
-# 关闭数据库连接
-db.close()
-'''
+from web.template import ALLOWED_AST_NODES
+ALLOWED_AST_NODES.append('Constant')
 
 render = web.template.render('templates/')
+# MySql-以List形式输出
+db = pymysql.connect("localhost", "root", "root", "moviesite",  cursorclass=pymysql.cursors.DictCursor)
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
 
 urls = (
     '/', 'index'
@@ -24,9 +17,9 @@ urls = (
 
 class index:
     def GET(self):
-        cursor.execute("SELECT move")
+        cursor.execute("select * from movie")
         movies = cursor.fetchall()
-        # movies = db.select('movie')
+        print(movies)
         return render.index(movies)
 
 
